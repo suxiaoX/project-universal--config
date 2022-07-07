1a67e8edb8f6504a63b552a5378bc594f1932c50
suxiao <suxiao@mixiaojin.com>
2022-07-07 14:38:23 +0800

    chore: init2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,27 @@
-modele.exports = {
-  extends: ['base-fe/react']
+/* eslint-disable */
+module.exports = {
+  // "env": {
+  //     "browser": true,
+  //     "es2021": true
+  // },
+  "extends": [
+    "base-fe/react"
+    // "eslint:recommended",
+    // "plugin:react/recommended",
+    // "plugin:@typescript-eslint/recommended"
+  ],
+  // "parser": "@typescript-eslint/parser",
+  // "parserOptions": {
+  //     "ecmaFeatures": {
+  //         "jsx": true
+  //     },
+  //     "ecmaVersion": "latest",
+  //     "sourceType": "module"
+  // },
+  // "plugins": [
+  //     "react",
+  //     "@typescript-eslint"
+  // ],
+  "rules": {
+  }
 }
---
+++ .versionrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+  // skip: [],
+  "types": [
+    {
+      "type": "bug",
+      "section": "针对特别bug"
+    },
+    {
+      "type": "feat",
+      "section": "新功能"
+    },
+    {
+      "type": "fix",
+      "section": "修补bug"
+    },
+    {
+      "type": "docs",
+      "section": "文档",
+      "hide": true
+    },
+    {
+      "type": "style",
+      "section": "格式",
+      "hide": true
+    },
+    {
+      "type": "refactor",
+      "section": "重构",
+      "hide": true
+    },
+    {
+      "type": "test",
+      "section": "测试",
+      "hide": true
+    },
+    {
+      "type": "chore",
+      "section": "构建过程或辅助工具的变动",
+      "hide": true
+    },
+    {
+      "type": "revert",
+      "section": "撤销之前的commit",
+      "hide": true
+    },
+    {
+      "type": "merge",
+      "section": "合并分支",
+      "hide": true
+    },
+    {
+      "type": "ci",
+      "section": "CI/CD",
+      "hide": true
+    }
+  ]
+}
